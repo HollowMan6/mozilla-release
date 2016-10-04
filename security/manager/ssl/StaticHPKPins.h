4c46630b954e5994f324a92761803ad55a3d5cb3
Ryan VanderMeulen <ryanvm@gmail.com>
2016-10-04 16:56:38 -0400

    Bug 1307530 - Bump the HPKP and HSTS expiration dates to 14-Feb, 2017. r=keeler, a=ritu

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1147,4 +1147,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1481978883086000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1487105336000000);
